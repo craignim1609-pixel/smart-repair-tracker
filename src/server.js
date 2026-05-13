01a2cac16bae5ccbcec41d672269faf3dfabba56
craignim1609-pixel <craignim1609@gmail.com>
2026-05-13 14:11:54 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(cors());
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // Health check
 app.get("/", (req, res) => {
