e352fda41aec7714eee33833aebf502f3557cad7
craignim1609-pixel <craignim1609@gmail.com>
2026-05-13 13:44:53 +0100

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,1 +1,16 @@
+// src/db/index.js
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected DB error", err);
+  process.exit(-1);
+});
+
+module.exports = pool;
 
