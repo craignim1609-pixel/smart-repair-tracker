917e553cae70b7f689ac4971a2fb88ab971cb875
craignim1609-pixel <craignim1609@gmail.com>
2026-05-13 14:18:45 +0100

    Update technicians.js

--- src/routes/technicians.js
+++ src/routes/technicians.js
@@ -4,20 +4,6 @@ const pool = require("../db");
 
 const router = express.Router();
 
-// GET /api/technicians
-router.get("/", async (req, res, next) => {
-  try {
-    const result = await pool.query("SELECT * FROM technicians ORDER BY id ASC");
-    res.json(result.rows);
-  } catch (err) {
-    next(err);
-  }
-});// src/routes/technicians.js
-const express = require("express");
-const pool = require("../db");
-
-const router = express.Router();
-
 /* -------------------------------------------------------
    GET ALL TECHNICIANS
 ------------------------------------------------------- */
@@ -128,21 +114,3 @@ router.delete("/:id", async (req, res, next) => {
 });
 
 module.exports = router;
-
-
-// POST /api/technicians
-router.post("/", async (req, res, next) => {
-  try {
-    const { name } = req.body;
-    const result = await pool.query(
-      "INSERT INTO technicians (name) VALUES ($1) RETURNING *",
-      [name]
-    );
-    res.status(201).json(result.rows[0]);
-  } catch (err) {
-    next(err);
-  }
-});
-
-module.exports = router;
-
